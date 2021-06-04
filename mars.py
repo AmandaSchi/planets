c3d23d2ed483dc23adb0c5cac9b1c30b226d7502
Amanda Schilling <amanda.schilling@ou.edu>
2021-06-04 13:55:55 -0500

    Read in data

--- mars.py
+++ mars.py
@@ -1,2 +1,6 @@
 # This code will analyze data we collect from Mars ice caps
 # Author: A Schilling, June 2021
+
+# Import pandas and read in data
+import pandas as pd
+data = pd.read_csv("directory/data.csv")
